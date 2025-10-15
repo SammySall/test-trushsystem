883d1f7d2a079d4f2ace1c8f211c7430721ab982
SammySall <167883651+SammySall@users.noreply.github.com>
2025-10-15 16:22:16 +0700

    set environment homepage all img to page

--- resources/views/layout/layoutuser.blade.php
+++ resources/views/layout/layoutuser.blade.php
@@ -8,6 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
     <link rel="stylesheet" href="{{ asset('css/header.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/homepage.css') }}">
 </head>
 
 <body class="d-flex flex-column min-vh-100">
@@ -30,12 +31,12 @@
             <div class="d-flex flex-column justify-content-center align-items-center mt-3 mt-lg-0">
                 <div class="d-flex justify-content-center align-items-center gap-2">
                     {{-- ปุ่มล็อตอิน --}}
-                    <a href="#" class="text-center">
+                    <a href="#" class="text-center me-3">
                         <img src="../img/menuuser/login.png" alt="login" class="img-fluid btn-hover-effect">
                     </a>
                     {{-- ปุ่มสมัครสมาชิก --}}
                     <a href="#" class="text-center">
-                        <img src="../img/menuuser/register.png" alt="login" class="img-fluid btn-hover-effect">
+                        <img src="../img/menuuser/register.png" alt="login" class="img-fluid btn-hover-effect" height="46.13px">
                     </a>
                 </div>
                 <div class="text-warn mt-2 text-center">
@@ -59,7 +60,7 @@
             <div class="title-footer">เทศบาลตำบลท่าข้าม จังหวัดฉะเชิงเทรา</div>
             <div class="d-flex flex-column-reverse flex-md-row justify-content-center align-items-center gap-3 pt-2">
                 {{-- ที่อยู่ --}}
-                <div class="footer-location">
+                <div class="footer-location me-3">
                     <img src="../img/menuuser/Map-icon.png" alt="home">
                     122 หมู่ 3 ตำบลท่าข้าม อำเภอบางประกง จังหวัดฉะเชิงเทรา 24130
                 </div>
