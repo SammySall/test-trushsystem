15b7427a61949a55698087ec4d283d5b0b597685
SammySall <nattapak.jusuwansiri@gmail.com>
2025-12-25 18:52:09 +0700

    line

--- app/Http/Controllers/EmergencyController.php
+++ app/Http/Controllers/EmergencyController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Emergency;
 use Illuminate\Support\Facades\Storage;
+use App\Http\Controllers\LineMessagingController;
+
 
 class EmergencyController extends Controller
 {
